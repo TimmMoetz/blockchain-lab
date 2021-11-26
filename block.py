0d8496d9a4431c42d2dd5eca2eb1bd34ad0d85ef
Jan Cwik <jc024@hdm-stuttgart.de>
2021-11-26 21:26:49 +0100

    test

--- block.py
+++ block.py
@@ -1,30 +1,74 @@
+from datetime import datetime
 import hashlib
 import json
-from textwrap import dedent
-from uuid import uuid4
-import jsonpickle
-from flask import Flask
-from urllib.parse import urlparse
-from Crypto.PublicKey import RSA
-from Crypto.Signature import *
-from time import time
+from abc import ABC, abstractmethod
+
+BLOCKCHAIN_DIR = 'blocks/'
 
 
-class Block (object):
-    def __init__(self, transactions, time, index):
-        self.index = index
-        self.transactions = transactions
-        self.time = time
-        self.prev = ''
-        self.hash = self.calculateHash()
+class Serializable(ABC):
+    def serialize(self):
+        return json.dumps(self.to_dict(),
+                          sort_keys=True).encode("utf-8")
 
-    def calculateHash(self):
-        hashTransactions = ""
+    @abstractmethod
+    def to_dict(self):
+        pass
 
-        for transaction in self.transactions:
-            hashTransactions += transaction.hash
 
-        hashString = str(self.time) + hashTransactions + \
-            self.prev + str(self.index)
-        hashEncoded = json.dumps(hashString, sort_keys=True).encode()
-        return hashlib.sha256(hashEncoded).hexdigest()
+class Transaction(Serializable):
+    def __init__(self, source=None, target=None, amount=0):
+        self.__source = source
+        self.__target = target
+        self.__amount = amount
+        self.__timestamp = datetime.now()
+
+    def to_dict(self):
+        return {
+            "source": self.__source,
+            "target": self.__target,
+            "amount": self.__amount,
+            "timestamp": self.__timestamp.strftime("%m/%d/%Y, %H:%M:%S")
+        }
+
+
+class Block(Serializable):
+    def __init__(self, pred=None):
+        self.predecessor = pred
+        self.transactions = list()
+
+    def add_transaction(self, t):
+        self.transactions.append(t)
+
+    def to_dict(self):
+        transactions = list()
+        for t in self. transactions:
+            transactions.append(t.to_dict())
+
+        return {
+            "predecessor": self.predecessor,
+            "transactions": transactions
+        }
+
+    def hash(self):
+        print("Erzeuge Hash für:", self.serialize())
+        return hashlib.sha256(self.serialize()).hexdigest()
+
+    def write_to_file(self, directory):
+        h = self.hash()
+        try:
+            with open(directory + "/" + str(h), "w") as file:
+                file.write(str(self.serialize()))
+        except EOFError:
+            print("...")
+
+
+def main():
+    b1 = Block(pred=None)
+    t1 = Transaction(source='Timme', target='Jan', amount=1000)
+    b1.add_transaction(t1)
+    b1.write_to_file("./blocks")
+
+
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -1,30 +1,18 @@
-from blockchain import *
-from block import *
-from transaction import *
-from time import time
-import pprint
+import hashlib
+from Block import Block, Transaction
 
-pp = pprint.PrettyPrinter(indent=4)
+dig = hashlib.sha256(b"hello world").hexdigest()
+print("Digest=" + str(dig))
+print("Digest (binary)=" + bin(int(dig, 16)))
 
-blockchain = Blockchain()
-transactions = [
-    """    {'hash': 1, 
-     "borrower": "Timm",
-     "lender": "Jan",
-     "amount": 100
-    }
-    """
-]
+t1 = Transaction("Peter", "Stephan", 100.0)
+t2 = Transaction("Stephan", "Peter", 5.0)
+b1 = Block(pred=None)
+b1.add_transaction(t1)
+b1.add_transaction(t2)
+b1_hash = b1.hash()
+b2 = Block(pred=b1_hash)
+b2_hash = b2.hash()
 
-
-block = Block(transactions, time(), 0)
-blockchain.addBlock(block)
-
-block = Block(transactions, time(), 1)
-blockchain.addBlock(block)
-
-block = Block(transactions, time(), 2)
-blockchain.addBlock(block)
-
-pp.pprint(blockchain.chainJSONencode())
-print("Länge: ", len(blockchain.chain))
+b1.write_to_file("./blocks")
+b2.write_to_file("./blocks")
